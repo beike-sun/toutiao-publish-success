16d7ce50ab9bfe79ad40a350386538fad2601611
songlihong <1876766912@qq.com>
2020-04-20 14:52:53 +0800

    安装并导入了第三方组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// 导入一个第三方组件库
+import ElementUI from 'element-ui'
+// 导入第三方组件库中的样式
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
