f27abb2148c82cc72fe3bd989344ed5296e4a631
songlihong <1876766912@qq.com>
2020-05-01 00:02:18 +0800

    完成点击图片显示弹出层

--- src/views/pubLish/components/upload-cover.vue
+++ src/views/pubLish/components/upload-cover.vue
@@ -11,16 +11,18 @@
       </div>
       <!-- 弹层框 -->
       <el-dialog
-        title="提示"
         :visible.sync="dialogVisible"
-        width="30%"
         append-to-body=true
         >
-        <span>这是一段信息</span>
-        <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-        </span>
+  <el-tabs  type="card" >
+    <el-tab-pane label="素材库" name="first">用户管理</el-tab-pane>
+    <el-tab-pane label="上传图片" name="second">配置管理</el-tab-pane>
+  </el-tabs>
+  <div class="upload-image"></div>
+    <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
         </el-dialog>
   </div>
 </template>
@@ -44,11 +46,18 @@ export default {
 <style>
 .cover-wrap{
     width: 180px;
-    height: 160px;
+    height:180px;
     border: 1px green dashed;
+    background: url('pic_bg.png');
 }
 .cover-image{
     width: 100%;
     height: 100%;
+}
+.upload-image{
+  width: 200px;
+  height: 200px;
+  border: 1px dashed gray;
+  margin-left: 200px;
 }
 </style>
