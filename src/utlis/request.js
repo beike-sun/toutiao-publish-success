90e199c05596852f176c9184a333494fe8e19f8e
songlihong <1876766912@qq.com>
2020-05-02 12:41:08 +0800

    设置响应拦截器

--- src/utlis/request.js
+++ src/utlis/request.js
@@ -1,6 +1,8 @@
 // 基于axios分装的请求模块
 import axios from 'axios'
 import JSONbig from 'json-bigint'
+import router from '@/router'
+import { Message } from 'element-ui'
 // 创建一个axios实例request.相当于复制了一个axios.通过这个实例发请求
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/',
@@ -31,5 +33,28 @@ request.interceptors.request.use(function (config) {
   return config
 }, function (error) {
   // Do something with request error
+  return Promise.reject(error)
+})
+// 响应拦截器
+request.interceptors.response.use(function (response) {
+  // 成功的响应在这里处理
+  return response
+}, function (error) {
+// 失败的响应在这里处理
+const { status } = error.response
+if (status === 401) {
+  window.localStorage.removeItem('user')
+  router.push('/login')
+  Message.error('登录状态无效，请重新登陆')
+} else if (status === 403) {
+  Message({
+    type: 'warning',
+    message: '没有操作权限'
+  })
+} else if (status === 400) {
+   Message.error('参数错误，请检查请求参数')
+} else if (status >= 500) {
+Message.error('服务端内部异常，请稍后重试')
+}
   return Promise.reject(error)
 })
